31cdb4ad2023a187f9e3efce63da812b8eb9f551
isapanda <koro.koro.panda.dokoeiku@gmail.com>
2021-02-04 15:17:30 +0000

    renew buttons.py

--- scripts/buttons.py
+++ scripts/buttons.py
@@ -2,21 +2,33 @@
 #!encoding: utf8
 
 import sys, rospy, math, os
-from std_msgs.msg import UInt16
+from pimouse_run_corridor.msg import ButtonValues
 from std_srvs.srv import Trigger, TriggerResponse
 
+dev_file = "/dev/rtswitch"
+button_values = [0,0,0]
+
 if __name__=='__main__':
-    devfile = "/dev/rtswitch1"
-    rospy.init_node('button')
-    pub = rospy.Publisher('/button_read', UInt16, queue_size =1)
+    rospy.init_node('buttons')
+    pub = rospy.Publisher('/button_values', ButtonValues, queue_size =1)
     rate = rospy.Rate(10)
-#    rospy.wait_for_service('/button_read')
+    
     while not rospy.is_shutdown():
-      try:
-          with open(devfile,'r') as f:
-              status = int(f.read())
-              pub.publish(status)
-#              rospy.loginfo(status)
-      except:
-          rospy.logerr("cannot read rtswitch1")
-      rate.sleep()
+
+     try:
+       for i in range(3): 
+         button = dev_file + str(i)  
+         with open(button,'r') as f:
+              button_values[i]=int((f.read())) 
+       
+       d = ButtonValues()
+       d.button0 = button_values[0]
+       d.button1 = button_values[1]
+       d.button2 = button_values[2]
+
+#       print(d)
+       pub.publish(d)
+     except:
+       rospy.logerr("cannot read rtswitchs")
+     
+     rate.sleep()
