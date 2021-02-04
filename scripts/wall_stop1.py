a642482335c3a87225c6c78d2d666d9ea61f5568
isapanda <koro.koro.panda.dokoeiku@gmail.com>
2021-02-04 15:58:17 +0000

    renew wall_stop1.py

--- scripts/wall_stop1.py
+++ scripts/wall_stop1.py
@@ -2,7 +2,7 @@
 #encoding: utf8
 import rospy, copy
 from geometry_msgs.msg import Twist
-from std_msgs.msg import UInt16
+from pimouse_run_corridor.msg import ButtonValues
 from std_srvs.srv import Trigger, TriggerResponse
 from pimouse_ros.msg import LightSensorValues
 
@@ -11,12 +11,12 @@ class WallStop():
         self.cmd_vel = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
 
         self.sensor_values = LightSensorValues()
-        self.button_status = 0
+        self.button_status = ButtonValues()
         self.START = 1
         self.sw_counter = 0
         self.state = 0
         rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
-        rospy.Subscriber('/button_read',UInt16,self.callback_button_status)
+        rospy.Subscriber('/button_values',ButtonValues,self.callback_button_status)
 
     def callback(self,messages):
         self.sensor_values = messages
@@ -28,7 +28,7 @@ class WallStop():
         rate = rospy.Rate(10)
         data = Twist()
         while not rospy.is_shutdown():
-            if self.button_status == UInt16(1):
+            if self.button_status.button1 == 1:
               self.START = 1
               self.sw_counter = self.sw_counter + 1
             else:
