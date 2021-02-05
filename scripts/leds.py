fef78a6bcb49b5df92a9169717f3199a4e8d3bad
isapanda <koro.koro.panda.dokoeiku@gmail.com>
2021-02-05 15:01:41 +0000

    create leds.py

---
+++ scripts/leds.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#!encoding: utf8
+
+import sys, rospy, math, os
+#from pimouse_run_corridor.msg import ButtonValues
+from pimouse_run_corridor.msg import LedValues
+from std_srvs.srv import Trigger, TriggerResponse
+
+
+class Leds():
+
+    def __init__(self):
+        self.led_values = LedValues()
+        self.ON = 1
+        self.OFF = 0
+        rospy.Subscriber('/led_values', LedValues, self.callback_leds)
+
+    def callback_leds(self,message):
+        self.switch_leds(message.green_value, message.blue_value, message.yellow_value, message.red_value)
+ 
+    def switch_leds(self,green_value, blue_value, yellow_value, red_value): 
+        try:
+           with open("/dev/rtled0",'w') as green,\
+                open("/dev/rtled1",'w') as blue,\
+                open("/dev/rtled2",'w') as yellow,\
+                open("/dev/rtled3",'w') as red:
+           
+                green.write(str(green_value) +"\n")
+                blue.write(str(blue_value) +"\n")
+                yellow.write(str(yellow_value) +"\n")
+                red.write(str(red_value) +"\n")
+        except:
+           rospy.logerr("cannot switch leds")
+
+
+if __name__=='__main__':
+    rospy.init_node('leds')
+    l = Leds()
+    rospy.spin()   
+# rate = rospy.Rate(10)
+   #while not rospy.is_shutdown():
